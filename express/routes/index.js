13de7143ddf7312c2656502609ce6ddbf607181a
Mohammad Usman Zafar <usmanzafar622@gmail.com>
2023-08-01 15:19:31 +0500

    Connecting Controllers with database using mongodb

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,34 +1,14 @@
-// const express = require("express");
-// const router = express.Router();
-// const userRouter = require("./userRouter");
-// const adminRouter = require("./adminRouter");
-// var jwt = require("jsonwebtoken");
+const { Router } = require("express");
+const router = Router();
 
-// function middleWare(req, res, next) {
-//   //console.log("My middleware");
-//   req.params.id += 1;
-//   //res.send("Middleware");
-//   next();
-// }
+const purchaserRoutes = require("../routes/purchaser");
+const sellerRoutes = require("../routes/seller");
+const adminRoutes = require("../routes/admin");
+const publicRoutes = require("../routes/public");
 
-// router.get("/", middleWare, function (req, res) {
-//   res.send("Hello, this is the root path");
-// });
-
-// // Router for global Middleware
-// router.get("/:id", middleWare, function (req, res) {
-//   res.send(req.params.id);
-// });
-
-// router.use("/users", userRouter);
-// router.use("/admin", adminRouter);
-
-// module.exports = router;
-
-const express = require("express");
-const router = express.Router();
-const userRouter = require("./userRouter");
-
-router.use("/users/", userRouter);
+router.use(publicRoutes);
+router.use("/admin", adminRoutes);
+router.use("/seller", sellerRoutes);
+router.use("/purchaser", purchaserRoutes);
 
 module.exports = router;
